d407dde672d89d3177daf28bef20f670aa3ffe72
Aurora01 <aurora@int0x10.com>
2015-05-19 19:17:43 +0100

    Added migration for default 'registered' group. Added static (collected using ./manage.py collectstatic)

---
+++ forums/migrations/0019_auto_20150519_1726.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import Group, Permission
+from django.db import migrations
+from django.contrib.contenttypes.models import ContentType
+
+from forums.models import Post
+
+
+def add_registered_group(apps, schema_editor):
+    group, created = Group.objects.get_or_create(name='registered')
+    if created:
+        content_type = ContentType.objects.get_for_model(Post)
+        p = Permission.objects.get(codename='add_post', content_type=content_type)
+        group.permissions.add(p)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('forums', '0018_auto_20150518_1634'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_registered_group),
+    ]
