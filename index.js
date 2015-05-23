ff17985a485849a4e993329798559b00443fac94
Tel-Aviv <tel@aviv.ml>
2015-05-23 15:49:46 -0400

    Finally adding things

---
+++ index.js
@@ -0,0 +1,51 @@
+var app = angular.module('descant', []);
+
+app.controller('pageCtrl', function() {
+	if (location.hash == '') {
+		location.hash = '/topics';
+	}
+	this.hashPage = location.hash.replace('#/', '');
+	this.currentPage = this.hashPage;
+	this.currentPage = this.hashPage;
+	this.isPage = function(page) {
+		return this.currentPage === page;
+	};
+	this.setPage = function(page) {
+		location.hash = '/'+page;
+		this.currentPage = page;
+	};
+});
+
+app.controller('topicsCtrl', function($http) {
+	var topicsCtrl = this;
+	$http.get("//django-descant.rhcloud.com/api/v0.1/topics").success(function (data) {
+		topicsCtrl.list = data;
+	});
+	this.showNTP = false;
+	this.toggleNTP = function() {
+		if (this.showNTP == true) {
+			this.showNTP = false;
+		} else {
+			this.showNTP = true;
+		}
+	};
+	this.mode = 'list';
+	this.openTopic = function(id) {
+		this.mode = 'topic';
+		this.id = id;
+		$http.get("//django-descant.rhcloud.com/api/v0.1/topics").success(function (data) {
+			topicsCtrl.postList = data;
+		});
+	};
+});
+
+app.controller('usersCtrl', function($http) {
+	var usersCtrl = this;
+	$http.get("download.php?api=http://django-descant.rhcloud.com/api/v0.1/users").success(function (data) {
+		usersCtrl.list = data;
+	});
+});
+
+app.controller('forumCtrl', function() {
+	
+});
