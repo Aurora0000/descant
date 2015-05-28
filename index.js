8036dc7771899e485a0f74e074740c9fc0323f29
Aurora01 <aurora@int0x10.com>
2015-05-28 15:05:56 +0100

    Added infinite scroll to replies, fixed logic.

--- index.js
+++ index.js
@@ -126,7 +126,6 @@ app.directive('topicList', function(descantConfig) {
 					}
 					var items = data['results'];
       				topicsCtrl.list = items;
-					topicsCtrl.offset = data['results'].length;
 					topicsCtrl.busy = false;
 				});
 				req.error(function(data) {
@@ -184,7 +183,7 @@ app.directive('tagTopicList', function(descantConfig) {
 						alert(items[i]);
         				topicsCtrl.list.push(items[i]);
       				}
-					topicsCtrl.offset = data['results'].length;
+					topicsCtrl.offset += data['results'].length;
 					topicsCtrl.busy = false;
 				});
 				req.error(function(data) {
@@ -203,7 +202,6 @@ app.directive('tagTopicList', function(descantConfig) {
 					}
 					var items = data['results'];
       				topicsCtrl.list = items;
-					topicsCtrl.offset += data['results'].length;
 					topicsCtrl.busy = false;
 				});
 				req.error(function(data) {
@@ -362,32 +360,69 @@ app.directive('postList', function(descantConfig) {
 		templateUrl: 'templates/posts/reply-list.html',
 		controller: function($http, $interval, $rootScope, $scope) {
 			var postsCtrl = this;
-			// Hacky. TODO: improve.
-
+			this.list = [];
+			this.busy = false;
+			this.offset = 0;
+			this.limit = 15;
+			this.end = false;
+			
 			this.updateList = function() {
-				var req = $http.get(descantConfig.backend + "/api/v0.1/topics/" + $scope.topicId + "/replies/");
+				if (postsCtrl.busy || postsCtrl.end) {
+					return;
+				}
+				postsCtrl.busy = true;
+				var req = $http.get(descantConfig.backend + "/api/v0.1/topics/" + $scope.topicId + "/replies/?limit=" + postsCtrl.limit.toString() + "&offset=" + postsCtrl.offset.toString());
 				req.success(function (data) {
-					postsCtrl.list = data;
-					postsCtrl.loaded = true;
+					if (data['results'].length == 0){
+						postsCtrl.end = true;
+						return;
+					}
+					var items = data['results'];
+      				for (var i = 0; i < items.length; i++) {
+						alert(items[i]);
+        				postsCtrl.list.push(items[i]);
+      				}
+					postsCtrl.offset += data['results'].length;
+					postsCtrl.busy = false;
 				});
 				req.error(function(data) {
-					postsCtrl.loaded = true;
-					postsCtrl.error = true;
+					postsCtrl.busy = false;
+					postsCtrl.end = true;
 				});
 			};
+			
+			this.refreshList = function() {
+				postsCtrl.busy = true;
+				postsCtrl.end = false;
+				var req = $http.get(descantConfig.backend + "/api/v0.1/topics/" + $scope.topicId + "/replies/?limit=" + postsCtrl.offset.toString() + "&offset=0");
+				req.success(function (data) {
+					if (data['results'].length == 0){
+						postsCtrl.end = true;
+						return;
+					}
+					var items = data['results'];
+      				postsCtrl.list = items;
+					postsCtrl.busy = false;
+				});
+				req.error(function(data) {
+					postsCtrl.busy = false;
+					postsCtrl.end = true;
+				});
+			};
+			
 			this.updateList();
-
+			
 			// Update once per minute.
-			this.stopUpdateList = $interval(this.updateList, 45000);
+			this.stopRefreshList = $interval(this.refreshList, 45000);
 
 			$rootScope.$on('topic:refresh', function() {
-				postsCtrl.updateList();
+				postsCtrl.refreshList();
 			});
 
 			// listen on DOM destroy (removal) event, and cancel the next UI update
 			// to prevent updating time after the DOM element was removed.
 			$rootScope.$on('$destroy', function() {
-				$interval.cancel(postsCtrl.stopUpdateList);
+				$interval.cancel(postsCtrl.stopRefreshList);
 			});
 
 		},
