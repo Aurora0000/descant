718982c800591321d95631e106b0b867b4f44c5a
Aurora01 <aurora@int0x10.com>
2015-06-08 17:43:23 +0100

    User Control Panel works (ugly and unintuitive though)

--- app.annotated.js
+++ app.annotated.js
@@ -1044,16 +1044,36 @@ controllerApp.controller('UserCPController', [
   '$location',
   'descantConfig',
   function ($http, $location, descantConfig) {
+    this.options = false;
     this.changeUser = function (new_username, current_password) {
-      alert('Submitting...');
       $http.post(descantConfig.backend + '/api/auth/username/', {
         'new_username': new_username,
         'current_password': current_password
       }).success(function (data) {
         $location.path('/');
       }).error(function (data) {
-        alert(data);
+        alert('Error! Is your current password correct?');
       });
+    };
+    this.changeEmail = function (new_mail) {
+      $http.patch(descantConfig.backend + '/api/auth/me/', { 'email': new_mail }).success(function (data) {
+        $location.path('/');
+      }).error(function (data) {
+        alert('Error! Is your current password correct?');
+      });
+    };
+    this.changePassword = function (new_password, current_password) {
+      $http.post(descantConfig.backend + '/api/auth/password/', {
+        'current_password': current_password,
+        'new_password': new_password
+      }).success(function (data) {
+        $location.path('/');
+      }).error(function (data) {
+        alert('Error! Is your current password correct?');
+      });
+    };
+    this.showOptions = function () {
+      this.options = true;
     };
   }
 ]);
--- controllers/routeControllers.js
+++ controllers/routeControllers.js
@@ -29,11 +29,33 @@ controllerApp.controller('ActivateController', function($http, descantConfig, $l
 });
 
 controllerApp.controller('UserCPController', function($http, $location, descantConfig) {
+	this.options = false;
+	
 	this.changeUser = function(new_username, current_password) {
 		$http.post(descantConfig.backend + "/api/auth/username/", {"new_username": new_username, "current_password": current_password}).success(function(data) {
 			$location.path('/');
 		}).error(function(data) {
-			alert(data);	
+			alert("Error! Is your current password correct?");
 		});
+	};
+	
+	this.changeEmail = function(new_mail) {
+		$http.patch(descantConfig.backend + "/api/auth/me/", {"email": new_mail}).success(function(data) {
+			$location.path('/');
+		}).error(function(data) {
+			alert("Error! Is your current password correct?");
+		});
+	};
+	
+	this.changePassword = function(new_password, current_password) {
+		$http.post(descantConfig.backend + "/api/auth/password/", {"current_password": current_password, "new_password": new_password }).success(function(data) {
+			$location.path('/');
+		}).error(function(data) {
+			alert("Error! Is your current password correct?");
+		});
+	};
+	
+	this.showOptions = function() {
+		this.options = true;
 	};
 });
