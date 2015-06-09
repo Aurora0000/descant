6d6f09b6b36d98f074b83aceefb571aafe8f8a97
Aurora01 <aurora@int0x10.com>
2015-06-09 20:28:16 +0100

    Added infinite scroll to users

--- static/descant/app.js
+++ static/descant/app.js
@@ -865,15 +865,35 @@ userListApp.directive('userList', function(descantConfig) {
 		templateUrl: 'templates/users/user-list.html',
 		controller: function($http) {
 			var usersCtrl = this;
-			var req = $http.get(descantConfig.backend + "/api/v0.1/users/");
-			req.success(function (data) {
-				usersCtrl.list = data;
-				usersCtrl.loaded = true;
-			});
-			req.error(function(data){
-				usersCtrl.loaded = true;
-				usersCtrl.error = true;
-			});
+			this.list = [];
+			this.busy = false;
+			this.offset = 0;
+			this.limit = 15;
+			this.end = false;
+			this.updateList = function() {
+				if (this.busy || this.end) {
+					return;
+				}
+				this.busy = true;
+				var req = $http.get(descantConfig.backend + "/api/v0.1/users/?limit=" + this.limit.toString() + "&offset=" + this.offset.toString());
+				req.success(function (data) {
+					if (data['results'].length == 0){
+						usersCtrl.end = true;
+						return;
+					}
+					var items = data['results'];
+      				for (var i = 0; i < items.length; i++) {
+        				usersCtrl.list.push(items[i]);
+      				}
+					usersCtrl.offset += data['results'].length;
+					usersCtrl.busy = false;
+				});
+				req.error(function(data) {
+					usersCtrl.busy = false;
+					usersCtrl.end = true;
+				});
+			};
+
 		},
 		controllerAs: 'users'
 	}
