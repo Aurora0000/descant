6db5caf4dbda1967bf5a3fdfca4dffbe140503ab
Aurora01 <aurora@int0x10.com>
2015-06-14 18:06:29 +0100

    Moved css for themes to styles/

--- static/descant/gulpfile.js
+++ static/descant/gulpfile.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp')
-var concat = require('gulp-concat')
-var uglify = require('gulp-uglify')
-var ngAnnotate = require('gulp-ng-annotate')
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var minifyCss = require('gulp-minify-css');
  
 gulp.task('js', function () {
   gulp.src(['vendor/*.js', 'index.js', 'config.js', 'services/*.js', 'directives/*.js', 'controllers/*.js', 'filters/*.js'])
